dd925cc3d3e295da6b33b9ace1f129fa11cd5bea
nka2001 <nandrle01@gmail.com>
2022-04-13 11:05:38 -0400

    CSC 311 GUI Created

--- src/main/java/group1/csc311hw3/PrimaryController.java
+++ src/main/java/group1/csc311hw3/PrimaryController.java
@@ -1,12 +1,44 @@
 package group1.csc311hw3;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
 
 public class PrimaryController {
 
     @FXML
+    private TextArea textArea;
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void openFile(ActionEvent event) {
+    }
+
+    @FXML
+    private void saveFile(ActionEvent event) {
+    }
+
+    @FXML
+    private void closeProgram(ActionEvent event) {
+    }
+
+    @FXML
+    private void undoType(ActionEvent event) {
+    }
+
+    @FXML
+    private void redoType(ActionEvent event) {
+    }
+
+    @FXML
+    private void testMe(KeyEvent event) {
+        
+        System.out.println(textArea.getText());
+        
     }
 }
